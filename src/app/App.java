431214ac806f048621607ce2e6ff5af9e8e1e6e8
lucasgomes14 <lucazmatehus14@gmail.com>
2023-03-06 15:15:16 -0300

    Department entity class

--- src/app/App.java
+++ src/app/App.java
@@ -1,7 +1,10 @@
 package app;
 
+import model.entities.Department;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Department department = new Department(1, "Books");
+		System.out.println(department);
     }
 }
