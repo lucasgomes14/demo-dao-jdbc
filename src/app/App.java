499c877538f34c06d2395277396128ecd8de40dd
lucasgomes14 <lucazmatehus14@gmail.com>
2023-03-06 15:32:22 -0300

    Seller entity class

--- src/app/App.java
+++ src/app/App.java
@@ -1,10 +1,15 @@
 package app;
 
+import java.util.Date;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class App {
     public static void main(String[] args) throws Exception {
         Department department = new Department(1, "Books");
-		System.out.println(department);
+		Seller seller = new Seller(1, "Bob", "bob@gmail.com", new Date(), 3000.00, department);
+
+		System.out.println(seller);
     }
 }
